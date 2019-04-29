663e779a3ce80b75580d6aecd48ff43d24f234bc
hsh803 <48030964+hsh803@users.noreply.github.com>
2019-04-29 20:41:10 +0200

    first upload from scratch

---
+++ tip_calculator.js
@@ -0,0 +1,33 @@
+function tipCal(){
+
+let totalNum = document.getElementById("total").value;
+let personNum = document.getElementById("person").value;
+let tipNum = document.getElementById("tip").value;
+let payNum = (totalNum * tipNum) / personNum;
+let total = Math.round(payNum)
+
+
+if ((totalNum === "" && personNum === "") || (isNaN(totalNum) === true && isNaN(personNum) === true)){
+   document.getElementById("warning1").innerHTML = "Please fill number.";
+   document.getElementById("warning2").innerHTML = "Please fill number.";
+   document.getElementById("total").value = "";
+   document.getElementById("person").value = "";
+ }
+else if (totalNum === "" || isNaN(totalNum) === true){
+   document.getElementById("warning1").innerHTML = "Please fill number.";
+   document.getElementById("total").value = "";
+ }
+else if (personNum === "" || isNaN(personNum) === true){
+   document.getElementById("warning2").innerHTML = "Please fill number.";
+   document.getElementById("person").value = "";
+ }
+else {
+    document.getElementById("total").value = "";
+    document.getElementById("person").value = "";
+    document.getElementById("warning1").innerHTML = "";
+    document.getElementById("warning2").innerHTML = "";
+    document.getElementById('result').innerHTML = alert(total + " each person");
+
+ }
+
+}
