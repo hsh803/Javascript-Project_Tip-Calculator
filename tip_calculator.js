1946e8a122be005dae7ba361d331a9589337c565
Hanna Han <48030964+hsh803@users.noreply.github.com>
2020-03-25 08:50:15 +0100

    Add files via upload

--- tip_calculator.js
+++ tip_calculator.js
@@ -1,33 +1,35 @@
 function tipCal(){
 
-let totalNum = document.getElementById("total").value;
-let personNum = document.getElementById("person").value;
-let tipNum = document.getElementById("tip").value;
-let payNum = (totalNum * tipNum) / personNum;
-let total = Math.round(payNum)
-let orgNum = (totalNum/personNum) + total;
+let totalAmount = document.getElementById("total").value;
+let personAmount = document.getElementById("person").value;
+let tipPercent = document.getElementById("tip").value;
+let tipAmount = (totalAmount * tipPercent) / personAmount;
+//let total = Math.round(tipAmount)
+let totalPay = (totalAmount/personAmount) + tipAmount;
 
-if ((totalNum === "" && personNum === "") || (isNaN(totalNum) === true && isNaN(personNum) === true)){
+if ((totalAmount === "" && personAmount === "") || (isNaN(totalAmount) === true && isNaN(personAmount) === true)){
    document.getElementById("warning1").innerHTML = "Please fill number.";
    document.getElementById("warning2").innerHTML = "Please fill number.";
    document.getElementById("total").value = "";
    document.getElementById("person").value = "";
- } 
-else if (totalNum === "" || isNaN(totalNum) === true){
+ }
+else if (totalAmount === "" || isNaN(totalAmount) === true){
    document.getElementById("warning1").innerHTML = "Please fill number.";
+   document.getElementById("warning2").innerHTML = "";
    document.getElementById("total").value = "";
  }
 
-else if (personNum === "" || isNaN(personNum) === true){
+else if (personAmount === "" || isNaN(personAmount) === true){
+   document.getElementById("warning1").innerHTML = "";
    document.getElementById("warning2").innerHTML = "Please fill number.";
    document.getElementById("person").value = "";
- } 
+ }
   else {
     document.getElementById("total").value = "";
     document.getElementById("person").value = "";
     document.getElementById("warning1").innerHTML = "";
     document.getElementById("warning2").innerHTML = "";
-    document.getElementById('result').innerHTML = alert("Each person pay " + orgNum + " (Tip:" + total + " each person)"); 
+    alert(totalPay + "/person"  + " (Tip: " + tipAmount + "/person)");
  }
-  
+
 }
