bd2c1f38d2b09ab1cf833ecadba1b7a08cbb8c84
Hanna Han <48030964+hsh803@users.noreply.github.com>
2020-04-16 10:44:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,27 +4,26 @@ let totalAmount = document.getElementById("total").value;
 let personAmount = document.getElementById("person").value;
 let tipPercent = document.getElementById("tip").value;
 let tipAmount = (totalAmount * tipPercent) / personAmount;
-//let total = Math.round(tipAmount)
 let totalPay = (totalAmount/personAmount) + tipAmount;
 
-if ((totalAmount === "" && personAmount === "") || (isNaN(totalAmount) === true && isNaN(personAmount) === true)){
-   document.getElementById("warning1").innerHTML = "Please fill number.";
-   document.getElementById("warning2").innerHTML = "Please fill number.";
+if (totalAmount === "" && personAmount === ""){
+   document.getElementById("warning1").innerHTML = "Please fill numbers.";
+   document.getElementById("warning2").innerHTML = "Please fill numbers.";
    document.getElementById("total").value = "";
    document.getElementById("person").value = "";
  }
-else if (totalAmount === "" || isNaN(totalAmount) === true){
+else if (totalAmount === ""){
    document.getElementById("warning1").innerHTML = "Please fill number.";
    document.getElementById("warning2").innerHTML = "";
    document.getElementById("total").value = "";
  }
 
-else if (personAmount === "" || isNaN(personAmount) === true){
+else if (personAmount === ""){
    document.getElementById("warning1").innerHTML = "";
    document.getElementById("warning2").innerHTML = "Please fill number.";
    document.getElementById("person").value = "";
  }
-  else {
+else {
     document.getElementById("total").value = "";
     document.getElementById("person").value = "";
     document.getElementById("warning1").innerHTML = "";
