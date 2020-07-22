d6d57462e12d1fd48c29553542ee951c616cfdde
Hanna Han <48030964+hsh803@users.noreply.github.com>
2020-07-22 19:05:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,32 +3,18 @@ function tipCal(){
 let totalAmount = document.getElementById("total").value;
 let personAmount = document.getElementById("person").value;
 let tipPercent = document.getElementById("tip").value;
-let tipAmount = (totalAmount * tipPercent) / personAmount;
+let tipAmount = (totalAmount * (tipPercent/100)) / personAmount;
 let totalPay = (totalAmount/personAmount) + tipAmount;
 
-if (totalAmount === "" && personAmount === ""){
-   document.getElementById("warning1").innerHTML = "Please fill numbers.";
-   document.getElementById("warning2").innerHTML = "Please fill numbers.";
-   document.getElementById("total").value = "";
-   document.getElementById("person").value = "";
- }
-else if (totalAmount === ""){
-   document.getElementById("warning1").innerHTML = "Please fill number.";
-   document.getElementById("warning2").innerHTML = "";
-   document.getElementById("total").value = "";
+if (totalAmount === "" || personAmount === "" || tipPercent === ""){
+   alert("Please fill numbers")
  }
 
-else if (personAmount === ""){
-   document.getElementById("warning1").innerHTML = "";
-   document.getElementById("warning2").innerHTML = "Please fill number.";
-   document.getElementById("person").value = "";
- }
 else {
     document.getElementById("total").value = "";
     document.getElementById("person").value = "";
-    document.getElementById("warning1").innerHTML = "";
-    document.getElementById("warning2").innerHTML = "";
-    alert(totalPay + "/person"  + " (Tip: " + tipAmount + "/person)");
+    document.getElementById("tip").value = "";
+    document.getElementById("cal").innerHTML = totalPay + " /person";    
  }
 
 }
